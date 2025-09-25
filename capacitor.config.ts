e51cd79a619845a4e255c5d652ac0cf85cec5d51
Nicolas <nicolas.rg2004@aluno.ifsc.edu.br>
2025-09-25 16:24:47 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tenis-amador-app',
+  webDir: 'www'
+};
+
+export default config;
